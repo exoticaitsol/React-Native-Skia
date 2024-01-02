548824a028c52079d0ddda42570209a8e7165e1b
William Candillon <wcandillon@gmail.com>
2024-01-02 08:15:55 +0100

    :wrench:

--- example/src/Examples/FrostedCard/FrostedCard.tsx
+++ example/src/Examples/FrostedCard/FrostedCard.tsx
@@ -9,6 +9,7 @@ import {
   Blur,
   usePathValue,
   Shader,
+  toMatrix3,
 } from "@shopify/react-native-skia";
 import { useDerivedValue } from "@shopify/react-native-skia/src/external/reanimated/moduleWrapper";
 import React from "react";
@@ -17,6 +18,8 @@ import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import { useSharedValue, withSpring } from "react-native-reanimated";
 
 import { frag } from "../../components/ShaderLib";
+
+import { Core } from "./Core";
 
 const { width, height } = Dimensions.get("window");
 const CARD_WIDTH = width * 0.9;
@@ -44,22 +47,17 @@ const springConfig = (velocity: number) => {
 };
 
 const source = frag`
-uniform mat4 m4;
+uniform mat3 m3;
+uniform vec2 resolution;
 
-vec4 main(vec2 fragCoord)
+${Core}
+
+vec4 main(vec2 xy)
 {
-    vec2 pos = (m4 * vec4(fragCoord, 1.0, 1.0)).xy; // Apply transformation to fragment position
-    float radius = 100; // Radius of the circle
-    vec2 center = vec2(${(width / 2).toFixed(1)}, ${(height / 2).toFixed(
-  1
-)}); // Center of the circle
-
-    // Check if the current fragment is inside the circle
-    if (length(pos - center) < radius) {
-        return vec4(0.3, 0.6, 1.0, 1.0); // Inside the circle (orange color)
-    } else {
-        return vec4(0.0, 0.0, 0.0, 1.0); // Outside the circle (transparent)
-    }
+  Context ctx = Context(TRANSPARENT, xy, resolution);
+  ctx.p = project(ctx.p, m3);
+  drawRect(ctx, vec4(0., 0., 300., 300.), createFill(vec4(.3, .6, 1., 1.)));
+  return ctx.color;
 }
 `;
 
@@ -78,28 +76,18 @@ export const FrostedCard = () => {
       rotateY.value = withSpring(0, springConfig(velocityX / sf));
     });
 
-  const clip = usePathValue((path) => {
-    "worklet";
-    path.addRRect(rrct);
-    path.transform(
-      processTransform3d([
-        { translate: [width / 2, height / 2] },
-        { perspective: 300 },
-        { rotateX: rotateX.value },
-        { rotateY: rotateY.value },
-        { translate: [-width / 2, -height / 2] },
-      ])
-    );
-  });
   const uniforms = useDerivedValue(() => {
     return {
-      m4: processTransform3d([
-        { translate: [width / 2, height / 2] },
-        { perspective: 300 },
-        { rotateX: rotateX.value },
-        { rotateY: rotateY.value },
-        { translate: [-width / 2, -height / 2] },
-      ]),
+      resolution: [width, height],
+      m3: toMatrix3(
+        processTransform3d([
+          // { translate: [width / 2, height / 2] },
+          //          { perspective: 300 },
+          // { rotateX: rotateX.value },
+          // { rotateY: rotateY.value },
+          // { translate: [-width / 2, -height / 2] },
+        ])
+      ),
     };
   });
   return (
