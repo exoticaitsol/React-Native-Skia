8241bd7478fe0223cec825100fd5452b5c3cc93b
William Candillon <wcandillon@gmail.com>
2024-03-16 21:41:43 +0100

    :wrench:

--- example/src/Examples/Matrix/Matrix2.tsx
+++ example/src/Examples/Matrix/Matrix2.tsx
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import type {
   DataModule,
   SkFont,
   SkImage,
-  SkSize,
+  SkSurface,
 } from "@shopify/react-native-skia";
 import {
   Canvas,
   useFont,
-  useTexture,
   Image,
   Glyphs,
   vec,
@@ -17,7 +16,9 @@ import {
   drawAsImage,
   Skia,
 } from "@shopify/react-native-skia";
-import { useWindowDimensions, Image as RNImage } from "react-native";
+import { Image as RNImage, Dimensions } from "react-native";
+import type { SharedValue } from "react-native-reanimated";
+import { runOnUI, useSharedValue } from "react-native-reanimated";
 
 export const COLS = 15;
 export const ROWS = 30;
@@ -25,59 +26,51 @@ export const ROWS = 30;
 const cols = new Array(COLS).fill(0).map((_, i) => i);
 const rows = new Array(ROWS).fill(0).map((_, i) => i);
 const pos = vec(0, 0);
+const size = Dimensions.get("window");
+const symbol = { width: size.width / COLS, height: size.height / ROWS };
+const paint = Skia.Paint();
+paint.setColor(Skia.Color("green"));
 
-const resolveAsset = async (source: DataModule, fontSize: number) => {
-  const uri =
-    typeof source === "number"
-      ? RNImage.resolveAssetSource(source).uri
-      : source.default;
-  const data = await Skia.Data.fromURI(uri);
-  const typeface = Skia.Typeface.MakeFreeTypeFaceFromData(data)!;
-  return Skia.Font(typeface, fontSize);
-};
-
-const drawTexture = async (size: SkSize) => {
-  const symbol = { width: size.width / COLS, height: size.height / ROWS };
-  const font = await resolveAsset(
-    require("./matrix-code-nfi.otf"),
-    symbol.height
-  );
+const drawTexture = (
+  surface: SharedValue<SkSurface | null>,
+  texture: SharedValue<SkImage | null>,
+  font: SkFont
+) => {
+  "worklet";
   const symbols = font.getGlyphIDs("abcdefghijklmnopqrstuvwxyz");
-  return drawAsImage(
-    <Group>
-      {cols.map((_i, i) =>
-        rows.map((_j, j) => {
-          const x = i * symbol.width;
-          const y = j * symbol.height;
-          return (
-            <Glyphs
-              key={`${i}-${j}`}
-              x={x + symbol.width / 4}
-              y={y + symbol.height}
-              font={font}
-              glyphs={[{ id: symbols[(i + j) % symbols.length], pos }]}
-              color="green"
-            />
-          );
-        })
-      )}
-    </Group>,
-    size
+  if (surface.value === null) {
+    surface.value = Skia.Surface.MakeOffscreen(size.width, size.height)!;
+  }
+  const canvas = surface.value.getCanvas();
+  canvas.clear(Skia.Color("black"));
+  cols.forEach((_i, i) =>
+    rows.forEach((_j, j) => {
+      const x = i * symbol.width;
+      const y = j * symbol.height;
+      canvas.drawGlyphs(
+        [symbols[(i + j) % symbols.length]],
+        [pos],
+        x + symbol.width / 4,
+        y + symbol.height,
+        font,
+        paint
+      );
+    })
   );
+  surface.value.flush();
+  texture.value = surface.value.makeImageSnapshot();
 };
 
 interface SceneProps {
-  size: SkSize;
+  font: SkFont;
 }
 
-const Scene = ({ size }: SceneProps) => {
-  const [texture, setTexture] = useState<null | SkImage>(null);
+const Scene = ({ font }: SceneProps) => {
+  const surface = useSharedValue<SkSurface | null>(null);
+  const texture = useSharedValue<SkImage | null>(null);
   useEffect(() => {
-    (async () => {
-      const tex = await drawTexture(size);
-      setTexture(tex);
-    })();
-  });
+    runOnUI(drawTexture)(surface, texture, font);
+  }, [font, surface, texture]);
   return (
     <Image
       image={texture}
@@ -90,10 +83,13 @@ const Scene = ({ size }: SceneProps) => {
 };
 
 export const Matrix = () => {
-  const { width, height } = useWindowDimensions();
+  const font = useFont(require("./matrix-code-nfi.otf"), symbol.height);
+  if (font === null) {
+    return null;
+  }
   return (
-    <Canvas style={{ width, height }}>
-      <Scene size={{ width, height }} />
+    <Canvas style={size}>
+      <Scene font={font} />
     </Canvas>
   );
 };
