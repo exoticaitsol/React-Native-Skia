123e7ea8db106fc252683a6d8f26fbbc822eeade
William Candillon <wcandillon@gmail.com>
2024-03-17 21:47:50 +0100

    :wrench:

--- example/src/Examples/Matrix/Matrix3.tsx
+++ example/src/Examples/Matrix/Matrix3.tsx
@@ -7,11 +7,15 @@ import {
   vec,
 } from "@shopify/react-native-skia";
 import { Dimensions } from "react-native";
-import { useDerivedValue, useSharedValue } from "react-native-reanimated";
+import {
+  useDerivedValue,
+  useSharedValue,
+  withDecay,
+} from "react-native-reanimated";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 
 const { width, height } = Dimensions.get("window");
-const size = 150;
+const size = 100;
 
 // Define the 8 vertices of a cube in 3D space
 const vertices = [
@@ -26,12 +30,12 @@ const vertices = [
 ].map(([x, y, z]) => [width / 2 + x * size, height / 2 + y * size, z * size]);
 
 const idx = [
-  [0, 1, 2],
-  [0, 2, 3], // Front face
+  //   [0, 1, 2],
+  //   [0, 2, 3], // Front face
   [1, 5, 6],
   [1, 6, 2], // Right face
-  [5, 4, 7],
-  [5, 7, 6], // Back face
+  //   [5, 4, 7],
+  //   [5, 7, 6], // Back face
   [4, 0, 3],
   [4, 3, 7], // Left face
   [3, 2, 6],
@@ -55,10 +59,15 @@ const colors = [
 export const Matrix = () => {
   const rotateX = useSharedValue(0);
   const rotateY = useSharedValue(0);
-  const gesture = Gesture.Pan().onChange((event) => {
-    rotateY.value += event.changeX / width;
-    rotateX.value -= event.changeY / height;
-  });
+  const gesture = Gesture.Pan()
+    .onChange((event) => {
+      rotateY.value += event.changeX / width;
+      rotateX.value -= event.changeY / height;
+    })
+    .onEnd(({ velocityX, velocityY }) => {
+      rotateY.value = withDecay({ velocity: velocityX / width });
+      rotateX.value = withDecay({ velocity: -velocityY / height });
+    });
   const transform = useDerivedValue(() => {
     return processTransform3d([
       { translate: [width / 2, height / 2] },
@@ -90,7 +99,7 @@ export const Matrix = () => {
       const z = (p0[2] + p1[2] + p2[2]) / 3;
       return [...triangle, z];
     });
-    values.sort((a, b) => b[3] - a[3]);
+    values.sort((a, b) => a[3] - b[3]);
     return values.map((triangle) => triangle.slice(0, 3)).flat();
   });
 
